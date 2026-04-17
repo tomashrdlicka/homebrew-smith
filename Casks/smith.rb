492e1da85e46c8355b6f99beff750a564cf95c17
Tomas Hrdlicka <60137254+tomashrdlicka@users.noreply.github.com>
2026-04-17 20:51:16 +0200

    chore: update cask formula for v0.3.10

--- Casks/smith.rb
+++ Casks/smith.rb
@@ -1,6 +1,6 @@
 cask "smith" do
-  version "0.3.8"
-  sha256 "2da10ce445c9b407d26be940fea98031c3d29b09cb9943ef62ec20fbd79670b9"
+  version "0.3.10"
+  sha256 "96757f5cada1049fa95e667795f7e20927062fc7d3e493a715b260cbc8139421"
 
   url "https://github.com/tomashrdlicka/homebrew-smith/releases/download/v#{version}/Smith-#{version}-arm64.dmg"
   name "Smith"
